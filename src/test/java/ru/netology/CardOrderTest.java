55ff92d46909e99356474a22afbcf89ad61ae244
Yuliana Smirnova <0387097@gmail.com>
2021-07-23 16:00:16 +0300

    WebDriverManager and .appveyor.yml added

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,21 +1,27 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CardOrderTest {
     private WebDriver driver;
 
     @BeforeAll
     static void setUpForAll(){
-        System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setUp(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
