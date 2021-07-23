a823fe1d42d06041e67cbbecf5129bbb86c86518
Yuliana Smirnova <0387097@gmail.com>
2021-07-23 17:20:17 +0300

    Tests added

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -41,4 +41,71 @@ public class CardOrderTest {
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldNotSendOrderWithoutData(){
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText().strip();
+        String expected = "Поле обязательно для заполнения";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotSendOrderWithNameInEnglish(){
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("Ivan Ivanov");
+        driver.findElement(By.cssSelector("[type=tel]")).sendKeys("+79000000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText().strip();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotSendOrderWithoutPhoneNumber(){
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("Иванов Иван");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText().strip();
+        String expected = "Поле обязательно для заполнения";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotSendOrderWithoutName() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type=tel]")).sendKeys("+79000000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText().strip();
+        String expected = "Поле обязательно для заполнения";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotSendOrderWithoutCheckBox(){
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[type=tel]")).sendKeys("+79000000000");
+        driver.findElement(By.className("button")).click();
+        String actual = driver.findElement(By.className("checkbox__text")).getCssValue("color");
+        String expected = "rgba(255, 92, 92, 1)";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotSendOrderWithShoterPhoneNumber(){
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[type=tel]")).sendKeys("+7900000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText().strip();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        Assertions.assertEquals(expected, actual);
+    }
 }
