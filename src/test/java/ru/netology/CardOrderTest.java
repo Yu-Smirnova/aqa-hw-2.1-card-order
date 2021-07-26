eeb5ecc6bab98b3f89ed4511e06bac5beea4245d
Yuliana Smirnova <0387097@gmail.com>
2021-07-26 11:12:11 +0300

    Tests update

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -22,6 +22,7 @@ public class CardOrderTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -32,19 +33,17 @@ public class CardOrderTest {
 
     @Test
     void shouldSendOrder(){
-        driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[type=text]")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[type=tel]")).sendKeys("+79000000000");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String actual = driver.findElement(By.className("paragraph")).getText().strip();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().strip();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void shouldNotSendOrderWithoutData(){
-        driver.get("http://localhost:9999/");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
         String actual = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText().strip();
@@ -54,7 +53,6 @@ public class CardOrderTest {
 
     @Test
     void shouldNotSendOrderWithNameInEnglish(){
-        driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[type=text]")).sendKeys("Ivan Ivanov");
         driver.findElement(By.cssSelector("[type=tel]")).sendKeys("+79000000000");
         driver.findElement(By.className("checkbox__box")).click();
@@ -66,7 +64,6 @@ public class CardOrderTest {
 
     @Test
     void shouldNotSendOrderWithoutPhoneNumber(){
-        driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[type=text]")).sendKeys("Иванов Иван");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
@@ -77,7 +74,6 @@ public class CardOrderTest {
 
     @Test
     void shouldNotSendOrderWithoutName() {
-        driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[type=tel]")).sendKeys("+79000000000");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
@@ -88,18 +84,16 @@ public class CardOrderTest {
 
     @Test
     void shouldNotSendOrderWithoutCheckBox(){
-        driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[type=text]")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[type=tel]")).sendKeys("+79000000000");
         driver.findElement(By.className("button")).click();
-        String actual = driver.findElement(By.className("checkbox__text")).getCssValue("color");
-        String expected = "rgba(255, 92, 92, 1)";
+        String actual = driver.findElement(By.cssSelector(".input_invalid[data-test-id=agreement] .checkbox__text")).getText().strip();
+        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void shouldNotSendOrderWithShoterPhoneNumber(){
-        driver.get("http://localhost:9999/");
+    void shouldNotSendOrderWithShorterPhoneNumber(){
         driver.findElement(By.cssSelector("[type=text]")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[type=tel]")).sendKeys("+7900000000");
         driver.findElement(By.className("checkbox__box")).click();
